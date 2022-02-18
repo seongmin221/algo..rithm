d84008b28c0c61f562d02442a59c18089da0f1d6
seongmin221 <seongmin221@naver.com>
2022-02-18 15:38:39 +0900

    refactor: 11046-동전0

--- BOJ/Greedy-algorithm/11046-동전0.py
+++ BOJ/Greedy-algorithm/11046-동전0.py
@@ -1,23 +1,3 @@
-# import timeit
-
-
-# import sys
-
-# n, k = map(int, sys.stdin.readline().split())
-# a = []
-# for i in range(n):
-#     a.append(int(sys.stdin.readline()))
-# start_time = timeit.default_timer()
-# min = 0
-# while k > 0:
-#     while k < a[n-1]:
-#         n -= 1
-#     k -= a[n-1]
-#     min += 1
-# print(min)
-
-
-
 import sys
 n, k = map(int, sys.stdin.readline().split())
 a = []
