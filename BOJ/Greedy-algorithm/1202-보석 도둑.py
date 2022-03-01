62112adbcff93d4b79c1d61ae9ae96c1e5a23219
seongmin221 <seongmin221@naver.com>
2022-03-01 16:30:25 +0900

    solved: 1026-보물

--- BOJ/Greedy-algorithm/1202-보석 도둑.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-n, k = map(int, sys.stdin.readline().split())
-
-j = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-c = [int(sys.stdin.readline()) for _ in range(n)]
-j.sort()
-c.sort()
-print(j, c)
