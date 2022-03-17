a55597ed56bc0e08fef2c57d78cec02be28eb56c
seongmin221 <seongmin221@naver.com>
2022-03-17 17:00:16 +0900

    solved: 1927-최소 힙

---
+++ BOJ/Data structure/1927-최소 힙.py
@@ -0,0 +1,15 @@
+import sys, heapq
+
+n = int(sys.stdin.readline().rstrip())
+mh = []
+k = []
+for i in range(n):
+    k.append(int(sys.stdin.readline().rstrip()))
+
+for i in k:
+    if i == 0 and len(mh) != 0:
+        print(heapq.heappop(mh))
+    elif i == 0 and len(mh) == 0:
+        print(0)
+    else:
+        heapq.heappush(mh, i)
