328e7df720d35571726cd54de0d6567b0c2e6ddb
seongmin221 <seongmin221@naver.com>
2022-03-20 14:42:47 +0900

    solved: 2606-바이러스

---
+++ BOJ/DFS&BFS/2606-바이러스.py
@@ -0,0 +1,29 @@
+import sys
+from queue import Queue
+
+
+# initial settings
+N = int(sys.stdin.readline().rstrip())
+M = int(sys.stdin.readline().rstrip())
+
+tmp = []
+graph = [[] for _ in range(N+1)]
+
+# for i in range(M):
+#     tmp.append(list(map(int, sys.stdin.readline())))
+tmp = [list(map(int, sys.stdin.readline().split())) for _ in range(M)]
+for i in tmp:
+    x, y = i[0], i[1]
+    graph[x].append(y)
+    graph[y].append(x)
+for tmp in graph:
+    tmp.sort()
+
+visitedD = []
+def DFS(graph, start):
+    visitedD.append(start)
+    for tmp in graph[start]:
+        if tmp not in visitedD:
+            DFS(graph, tmp)
+DFS(graph,1)
+print(len(visitedD)-1)
