c6a738c86a8bbdf9d98eba6682e8571b68262cb4
seongmin221 <seongmin221@naver.com>
2022-03-22 21:37:21 +0900

    corrected: 17298-오큰수

--- BOJ/Data structure/stack/17298-오큰수.py
+++ BOJ/Data structure/stack/17298-오큰수.py
@@ -2,3 +2,10 @@ import sys
 
 N = int(sys.stdin.readline().rstrip())
 arr = list(map(int, sys.stdin.readline().split()))
+answer = [-1] * N
+stack = [0]
+for i in range(1,N):
+    while stack and arr[stack[-1]] < arr[i]:
+        answer[stack.pop()] = arr[i]
+    stack.append(i)
+print(*answer)
