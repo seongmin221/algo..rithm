7a2d6f816564fa58b151d33e1269608a2bd28c36
seongmin221 <seongmin221@naver.com>
2022-03-26 19:27:31 +0900

    solved: 10844-쉬운 계단 수

--- BOJ/Dynamic Programming/10844-쉬운 계단 수.py
+++ BOJ/Dynamic Programming/10844-쉬운 계단 수.py
@@ -1,12 +1,14 @@
 import sys
 
 N = int(sys.stdin.readline().rstrip())
-count = [0 for _ in range(N+1)]
-for i in range(N+1):
-    if i == 1:
-        count[i] = 9
-    elif i == 2:
-        count[i] = 17
-    else:
-        count[i] = (count[i-1]-2) * 2 + 2
-print(count[N])
+count = [[0 for _ in range(10)] for _ in range(N+1)]
+
+count[1][0] = 0
+for j in range(1, 10):
+    count[1][j] = 1
+for i in range(2, N+1):
+    count[i][0] = count[i-1][1]
+    count[i][9] = count[i-1][8]
+    for j in range(1, 9):
+        count[i][j] = count[i-1][j-1] + count[i-1][j+1]
+print(sum(count[N])%1000000000)
