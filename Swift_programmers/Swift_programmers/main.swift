672baef26f20ed3034cbea09e05c1202586c38e4
seongmin221 <seongmin221@naver.com>
2023-03-27 14:13:29 +0900

    [INCORRECT] 체육복

--- Swift_programmers/Swift_programmers/main.swift
+++ Swift_programmers/Swift_programmers/main.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+//func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
+//    var lostStud = lost
+//    for stud in reserve {
+//        if let index = lostStud.firstIndex(of: stud) {
+//            lostStud.remove(at: index)
+//        } else if let index = lostStud.firstIndex(of: stud - 1) {
+//            lostStud.remove(at: index)
+//        } else if let index = lostStud.firstIndex(of: stud + 1) {
+//            lostStud.remove(at: index)
+//        }
+//    }
+//    return n - lostStud.count
+//}
+// 3~5,10,12,17~21,23~25 실패
+
+func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
+    
+}
+
+print(solution(3, [3], [1]))
+
+//n    lost    reserve    return
+//5    [2, 4]    [1, 3, 5]    5
+//5    [2, 4]    [3]    4
+//3    [3]    [1]    2
