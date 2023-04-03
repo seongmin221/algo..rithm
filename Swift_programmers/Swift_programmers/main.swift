f467c7498e897c08a4b5013c4d4f0b9ed96f8d1d
seongmin221 <seongmin221@naver.com>
2023-04-03 11:26:55 +0900

    [INCORRECT] 둘 만의 암호

--- Swift_programmers/Swift_programmers/main.swift
+++ Swift_programmers/Swift_programmers/main.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+//func solution(_ s:String, _ skip:String, _ index:Int) -> String {
+//    let alphaAscii = 97...122
+//    let skipAscii = skip.map { $0.asciiValue! }
+//    let allowedAscii = alphaAscii.filter { !skipAscii.contains(UInt8($0)) }
+//    let stringAscii = s.map { $0.asciiValue! }
+//    var result: [Int] = []
+//    for c in stringAscii {
+//        result.append(allowedAscii.firstIndex(of: ClosedRange<Int>.Element(c))!)
+//    }
+//    let allowedAlphabets = allowedAscii.map { UnicodeScalar($0)! }
+
+func solution(_ s:String, _ skip:String, _ index:Int) -> String {
+    let alphabets = [
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "m", "o", "p",
+        "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+    ]
+    let allowedAlphabets = alphabets.filter { !skip.contains($0) }
+    let allowedAlphabetsCount = allowedAlphabets.count
+    let string = s.map { $0.description }
+    var result = ""
+    for s in string {
+        let i = allowedAlphabets.firstIndex(of: s)!
+        result += allowedAlphabets[(i + index) % allowedAlphabetsCount]
+    }
+    return result
+}
+
+print(solution("aukks", "wbqd", 5))
+
+//s    skip    index    result
+//"aukks"    "wbqd"    5    "happy"
+
+//let alphabets = [
+//    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "m", "o", "p",
+//    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+//]
