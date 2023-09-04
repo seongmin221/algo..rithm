322794de76719c3e1bfa7898dc819fcc7ea47ce7
seongmin221 <seongmin221@naver.com>
2023-09-04 13:58:52 +0900

    changed file name to problem name

--- Swift_programmers/Swift_programmers/main.swift
+++ Swift_programmers/Swift_programmers/main.swift
@@ -2,36 +2,7 @@
 //  main.swift
 //  Swift_programmers
 //
-//  Created by 이성민 on 2023/04/27.
+//  Created by 이성민 on 2023/09/04.
 //
 
 import Foundation
-
-func solution(_ msg:String) -> [Int] {
-    var result = [Int]()
-    
-    let letters = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"
-    var dict = letters.components(separatedBy: " ")
-    var message = msg
-    
-    var temp = ""
-    var firstChar = ""
-    
-    while !message.isEmpty {
-        firstChar = String(message.removeFirst())
-        temp += firstChar
-        if dict.contains(temp) {
-            continue
-        } else {
-            dict.append(temp)
-            _ = temp.removeLast()
-            result.append(dict.firstIndex(of: temp)! + 1)
-            message = firstChar + message
-            temp = ""
-        }
-    }
-    
-    result.append(dict.firstIndex(of: temp)! + 1)
-    
-    return result
-}
