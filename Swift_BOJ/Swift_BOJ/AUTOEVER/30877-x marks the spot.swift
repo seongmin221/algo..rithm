ecf8e1d5ecc461653c4f23b0a4851833ea07e0b4
seongmin221 <seongmin221@naver.com>
2024-02-01 14:36:41 +0900

    [SOLVED] 30877 - x marks the spot

---
+++ Swift_BOJ/Swift_BOJ/AUTOEVER/30877-x marks the spot.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  Swift_BOJ
+//
+//  Created by 이성민 on 1/5/24.
+//
+
+import Foundation
+
+//let n = Int(readLine()!)!
+//var inputs: [(s: String, t: String)] = []
+//for _ in 0..<n {
+//    let input = readLine()!.split(separator: " ").map { String($0) }
+//    inputs.append((input[0], input[1]))
+//}
+//
+//var result = ""
+//for input in inputs {
+//    let p = input.s.lowercased().firstIndex(of: "x")!
+//    result += input.t[p].uppercased()
+//}
+//
+//print(result)
--- Swift_BOJ/Swift_BOJ/main.swift
+++ Swift_BOJ/Swift_BOJ/main.swift
@@ -2,25 +2,7 @@
 //  main.swift
 //  Swift_BOJ
 //
-//  Created by 이성민 on 1/5/24.
+//  Created by 이성민 on 2/1/24.
 //
 
 import Foundation
-
-let n = Int(readLine()!)!
-let arr = readLine()!
-    .split(separator: " ")
-    .map { Int($0)! }
-var dp = Array(repeating: 1, count: n)
-
-for k in 1..<n {
-    for j in 0..<k {
-        if arr[j] > arr[k] {
-            dp[k] += 1
-        }
-    }
-}
-
-print(dp)
-
-// 10 9 7 6 5
