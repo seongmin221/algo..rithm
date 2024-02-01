b73858609afbac771aa44cf4134f6ee7e953929f
seongmin221 <seongmin221@naver.com>
2024-02-01 18:30:27 +0900

    [SOLVED] 30876-Tren Del Fin Del Mundo

---
+++ Swift_BOJ/Swift_BOJ/AUTOEVER/30876-Tren Del Fin Del Mundo.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  Swift_BOJ
+//
+//  Created by 이성민 on 2/1/24.
+//
+
+import Foundation
+
+//let n = Int(readLine()!)!
+//
+//var southernMost: [Int] = [0, Int.max]
+//for _ in 0..<n {
+//    let trainStop = Array(readLine()!
+//        .split(separator: " ")
+//        .map { Int(String($0))! })
+//    if trainStop[1] < southernMost[1] {
+//        southernMost = trainStop
+//    }
+//}
+//
+//print(southernMost[0], southernMost[1])
