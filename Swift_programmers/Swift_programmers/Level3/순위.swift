f844bb51426c7812fc40be8183982d4d4f01fac5
seongmin221 <seongmin221@naver.com>
2024-04-19 15:18:36 +0900

    [BETTER SOLUTION] 순위

--- Swift_programmers/Swift_programmers/Level3/순위.swift
+++ Swift_programmers/Swift_programmers/Level3/순위.swift
@@ -7,39 +7,101 @@
 
 import Foundation
 
-func solution(_ n: Int, _ results: [[Int]]) -> Int {
-    
-    var allRecords: [Int: [Int]] = [:]
-    var map: [Int: [Int]] = [:]
-    for i in 1..<n+1 {
-        allRecords[i] = [0, 0]
-        map[i] = []
+//func solution(_ n: Int, _ results: [[Int]]) -> Int {
+//    
+//    var allRecords: [Int: [Int]] = [:]
+//    var map: [Int: [Int]] = [:]
+//    for i in 1..<n+1 {
+//        allRecords[i] = [0, 0]
+//        map[i] = []
+//    }
+//    for result in results { map[result[0]]!.append(result[1]) }
+//    
+//    for i in 1..<n+1 {
+//        var needVisit: [Int] = [i]
+//        var visited: [Int] = []
+//        
+//        while !needVisit.isEmpty {
+//            let node = needVisit.removeLast()
+//            
+//            if visited.contains(node) { continue }
+//            
+//            allRecords[i]![0] += 1
+//            allRecords[node]![1] += 1
+//            needVisit += map[node]!
+//            visited.append(node)
+//        }
+//    }
+//    
+//    var result = 0
+//    allRecords.values.forEach { record in
+//        if record.reduce(0, { $0 + $1 }) == n + 1 {
+//            result += 1
+//        }
+//    }
+//    
+//    return result
+//}
+
+
+class Player {
+    let index: Int
+    var beat: [Player] = []
+    var lost: [Player] = []
+
+    init(_ index: Int) {
+        self.index = index
     }
-    for result in results { map[result[0]]!.append(result[1]) }
-    
-    for i in 1..<n+1 {
-        var needVisit: [Int] = [i]
-        var visited: [Int] = []
-        
-        while !needVisit.isEmpty {
-            let node = needVisit.removeLast()
-            
-            if visited.contains(node) { continue }
-            
-            allRecords[i]![0] += 1
-            allRecords[node]![1] += 1
-            needVisit += map[node]!
-            visited.append(node)
+}
+
+extension Player {
+    func beat(_ player: Player) {
+        self.beat.append(player)
+    }
+
+    func lost(_ player: Player) {
+        self.lost.append(player)
+    }
+
+    func knownResultCount() -> Int {
+        func bfs(_ initialArr: [Player], beats: Bool) -> Int {
+            var count: Int = 0
+            var needVisit = initialArr
+            var visited: Set<Int> = []
+            while !needVisit.isEmpty {
+                let node = needVisit.removeLast()
+                if visited.contains(node.index) { continue }
+                count += 1
+                visited.insert(node.index)
+                if beats { needVisit += node.beat }
+                else { needVisit += node.lost }
+            }
+            return count
         }
+
+        return bfs(self.beat, beats: true) + bfs(self.lost, beats: false)
     }
-    
-    var result = 0
-    allRecords.values.forEach { record in
-        if record.reduce(0, { $0 + $1 }) == n + 1 {
+}
+
+func solution(_ n: Int, _ results: [[Int]]) -> Int {
+
+    var players: [Player] = []
+    for i in 1...n { players.append(.init(i)) }
+
+    for result in results {
+        let winner = players[result[0] - 1]
+        let loser = players[result[1] - 1]
+        winner.beat(loser)
+        loser.lost(winner)
+    }
+
+    var result: Int = 0
+    for player in players {
+        if player.knownResultCount() == n-1 {
             result += 1
         }
     }
-    
+
     return result
 }
 
